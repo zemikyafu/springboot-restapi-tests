9103073005cff379b04eddf0a83b87c9fa20e555
zemikyafu <zemikfcb@gmail.com>
2025-02-11 00:07:26 +0200

    create searchscan jpa method

--- src/main/java/com/hackerrank/api/repository/ScanRepository.java
+++ src/main/java/com/hackerrank/api/repository/ScanRepository.java
@@ -1,7 +1,14 @@
 package com.hackerrank.api.repository;
 
 import com.hackerrank.api.model.Scan;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ScanRepository extends JpaRepository<Scan, Long> {
+    @Query("SELECT s FROM Scan s WHERE s.domainName=:domainName ")
+    List<Scan> findAllByDomainName(@Param("domainName") String domainName, Sort sort);
 }
